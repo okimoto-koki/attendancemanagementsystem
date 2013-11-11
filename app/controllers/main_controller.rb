b03d7854d95a6361c034af0fc9542265662c84d5
okimoto <b211017@cc.it-hiroshima.ac.jp>
2013-11-11 16:19:26 +0900

    コメントいろいろ追加

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,12 @@
 class MainController < ApplicationController
 	before_filter :authenticate_user!
+	
+##基本的にメソッド名と同じviewが対応している
+
+##UserinfoとUserの違い => 出席時間や遅刻判定を持っているのがUserinfo  Deviceが作ったのがUser 両方にnameとnumberがある
 
 	def index
 		@userinfo = Userinfo.order("time DESC").limit(10).find_all_by_userId(current_user.id)	
-		$day = Time.now
-
 	end
 
 	def index_all
@@ -13,17 +15,32 @@ class MainController < ApplicationController
 
 	def new
 		@newUserinfo = Userinfo.new
+		#current_userは（多分）Deviceから
 		@newUserinfo.name = current_user.name
 		@newUserinfo.number = current_user.number
 		@newUserinfo.userId = current_user.id
 		@newUserinfo.time = Time.now
+
+		##データベースから判定対象の時間設定を持ってくる　今のところactiveで曜日が合っているもののみ
 		@timeCheck = TimeConfig.where(["activation = ? and youbi = ? ", 1, @newUserinfo.time.wday]).first
 		
-		#####遅刻判定部分true(1)で出席#####
-		if @newUserinfo.time.hour >= @timeCheck.start_hour && @newUserinfo.time.min >= @timeCheck.start_minitus then 
-			@newUserinfo.check = 1
-		else 
-			@newUserinfo.check = 0
+		#データベースから値を取ってこれなかった時用の例外処理
+		begin
+			#####遅刻判定部分true(1)で出席#####
+			#登録した時間のhourがstart_hour以上　かつ　end_hour以下　かつ　minitusがstart_minitus以上
+			if @newUserinfo.time.hour >= @timeCheck.start_hour && @newUserinfo.time.min >= @timeCheck.start_minitus && @newUserinfo.time.hour <= @timeCheck.end_hour then
+				#登録した時間のhourがend_hour以下　もしくは (end_hourと同じ　かつ　minitusがend_minitus以下) 
+				if @newUserinfo.time.hour < @timeCheck.end_hour || (@newUserinfo.time.hour == @timeCheck.end_hour && @newUserinfo.time.min < @timeCheck.end_minitus) then
+					@newUserinfo.check = true
+				else 
+					@newUserinfo.check = false
+				end
+			else
+				@newUserinfo.check = false
+			end
+		#データベースに値が無い場合、遅刻判定にnil(何もなし)を入れる
+		rescue
+			@newUserinfo.check = nil
 		end
 		###############################
 		@newUserinfo.save
@@ -53,7 +70,7 @@ class MainController < ApplicationController
 	end
 
 	def admin_find
-
+		@adminFind = User.order("number DESC").all
 	end
 
 	def admin_result
@@ -79,6 +96,8 @@ class MainController < ApplicationController
 		# $youbi = params[:start_time]
 		# @newTimeConfig.youbi = $youbi.weeks
 		############
+
+		#初期化
 		@newTimeConfig.activation = 0
 		@newTimeConfig.save
 	end
